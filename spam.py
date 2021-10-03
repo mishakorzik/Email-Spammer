3b00dd14937e2ff2e862d79bbb4fd0dee42e9c62
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-10-03 20:33:23 +0300

    Update spam.py

--- spam.py
+++ spam.py
@@ -16,7 +16,109 @@ def bomb():
 	os.system('clear')
 	print bcolors.OKGREEN + '''                            																											
 [>] Preparing and attacking ...
-[>] Pending ...
+[>] Pending ...'''
 
 
+print(bcolors.WARNING + '''												       
+\033[95mChoose a Mail Service:													
+1) Gmail  => Google									
+2) Yahoo  => Microsoft
+'''
+try:
+	server = raw_input(bcolors.WARNING + 'Mail Server: ' + bcolors.ENDC)
+	user = raw_input(bcolors.OKGREEN + 'Your Email: ' + bcolors.ENDC)
+	pwd = getpass.getpass(bcolors.FAIL + 'Password: ' + bcolors.ENDC)
+	to = raw_input(bcolors.OKGREEN + 'To: ' + bcolors.ENDC)
+	subject = raw_input(bcolors.LITBU + 'Subject (Optional): ' + bcolors.ENDC)
+	body = raw_input(bcolors.OKGREEN + 'Message: ' + bcolors.ENDC)
+	nomes = input(bcolors.OKGREEN + 'Number of Emails to send (1-5000): ' + bcolors.ENDC)
+	no = 0
+	message = 'From: ' + user + '\nSubject: ' + subject + '\n' + body
+except KeyboardInterrupt:
+	print bcolors.FAIL + '\nCanceled' + bcolors.ENDC
+	sys.exit()
+
+#Gmail
+
+if server == '1' or server == 'gmail' or server == 'Gmail':
+	bomb()
+	server = smtplib.SMTP("smtp.gmail.com", 587)
+	server.ehlo()
+	server.starttls()
+	try:
+		server.login(user, pwd)
+	except smtplib.SMTPAuthenticationError:
+		print bcolors.FAIL + '''Your Username or Password is incorrect, please try again using the correct credentials
+		Or you need to enable less secure apps
+		On Gmail: https://myaccount.google.com/lesssecureapps ''' + bcolors.ENDC
+		sys.exit()
+	while no != nomes:
+		try:
+			server.sendmail(user, to, message)
+			print bcolors.WARNING + 'Successfully sent ' + str(no+1) + ' emails' + bcolors.ENDC
+			no += 1
+			time.sleep(.8)
+		except KeyboardInterrupt:
+			print bcolors.FAIL + '\nCanceled' + bcolors.ENDC
+			sys.exit()
+		except:
+			print "Failed to Send "
+	server.close()
+	
+#Yahoo
+elif server == '2' or server == 'Yahoo' or server == 'yahoo':
+	server = smtplib.SMTP("smtp.mail.yahoo.com", 587)
+	bomb()
+	server.starttls()
+	try:
+		server.login(user, pwd)
+	except smtplib.SMTPAuthenticationError:
+		print bcolors.FAIL + '''Your Username or Password is incorrect, please try again using the correct credentials
+		Or you need to enable less secure apps
+		On Yahoo: https://login.yahoo.com/account/security?.scrumb=Tiby8TXUvJt#less-secure-apps
+		''' + bcolors.ENDC
+		sys.exit()
+	while no != nomes:
+		try:
+			server.sendmail(user, to, message)
+			print bcolors.WARNING + 'Successfully sent ' + str(no + 1) + ' emails' + bcolors.ENDC
+			no += 1
+			time.sleep(.8)
+		except KeyboardInterrupt:
+			print bcolors.FAIL + '\nCanceled' + bcolors.ENDC
+			sys.exit()
+		except:
+			print "Failed to Send"
+	server.close()
+	
+#Hotmail/Outlook
+elif server == '3' or server == 'outlook' or server == 'Outlook' or server == 'Hotmail' or server == 'hotmail':
+	server = smtplib.SMTP("smtp-mail.outlook.com", 587)
+	bomb()
+	server.ehlo()
+	server.starttls()
+	try:
+		server.login(user, pwd)
+	except smtplib.SMTPAuthenticationError:
+		print bcolors.FAIL + 'Your Username or Password is incorrect, please try again using the correct credentials' + bcolors.ENDC
+		sys.exit()
+	while no != nomes:
+		try:
+			server.sendmail(user, to, message)
+			print bcolors.WARNING + 'Successfully sent ' + str(no + 1) + ' emails' + bcolors.ENDC
+			no += 1
+			time.sleep(.8)
+		except KeyboardInterrupt:
+			print bcolors.FAIL + '\nCanceled' + bcolors.ENDC
+			sys.exit()
+		except smtplib.SMTPAuthenticationError:
+			print '\nThe username or password you entered is incorrect.'
+			sys.exit()
+		except:
+			print "Failed to Send "
+	server.close()
+	
+else:
+	print 'Works only with Gmail, Yahoo, Outlook and Hotmail.'
+	sys.exit()
 
