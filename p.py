e9e7621fe707fa1ac8432ad415f1e44ff40b0d71
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-10-11 21:23:26 +0300

    Add files via upload

---
+++ p.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+os.system("python2 src/logo.py")
+print("1. python1  -  .cpython-39.opt-2.pyc")
+print("2. python3  -  .cpython-310.opt-2.pyc")
+diya = input("Options: ")
+if diya == '1' or diya == '01' or diya == 'python1' or diya == 'Python1':
+	os.system("python src/custom_spam.cpython-39.opt-2.pyc")
+if diya == '2' or diya == '02' or diya == 'python3' or diya == 'Python3':
+	os.system("python3 src/custom_spam.cpython-310.opt-2.pyc")
