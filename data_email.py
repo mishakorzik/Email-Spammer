2ea3cbd360bd28c614d1aa8fd972c9fd7c8ed98e
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-10-11 21:40:23 +0300

    Update data_email.py

--- data_email.py
+++ data_email.py
@@ -1,10 +1,4 @@
 import os
 import sys
 
-print("1. python1  -  .cpython-39.opt-2.pyc")
-print("2. python3  -  .cpython-310.opt-2.pyc")
-diya = input("Options: ")
-if diya == '1' or diya == '01' or diya == 'python1' or diya == 'Python1':
-	os.system("python src/data_email.cpython-39.opt-2.pyc")
-if diya == '2' or diya == '02' or diya == 'python3' or diya == 'Python3':
-	os.system("python3 src/data_email.cpython-310.opt-2.pyc")
+os.system("python src/data_email.cpython-310.opt-2.pyc")
