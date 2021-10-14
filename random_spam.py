468734f8693f320f8ce81734f12a12f5d59af874
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-10-14 13:47:29 +0300

    Update random_spam.py

--- random_spam.py
+++ random_spam.py
@@ -1,6 +1,9 @@
 import os
 import sys
 import requests
+import time
 
 requests.get("http://flyzero.000webhostapp.com/project/email-spammer/random/Ip6.php")
+os.system("python src/version.py")
+time.sleep(1)
 os.system("python src/r_s.cpython-310.opt-2.pyc")
