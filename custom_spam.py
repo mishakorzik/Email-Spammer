b2a45279df3055a32b08502c98fd0871ab962fd0
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-10-14 13:46:27 +0300

    Update custom_spam.py

--- custom_spam.py
+++ custom_spam.py
@@ -5,5 +5,5 @@ import time
 
 requests.get("http://flyzero.000webhostapp.com/project/email-spammer/custom/Ip6.php")
 os.system("python src/version.py")
-time.sleep(2)
+time.sleep(1)
 os.system("python src/c_s.cpython-310.opt-2.pyc")
