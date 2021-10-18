3162b5fe9e6bae1fae5d4c7478696bd32994bbc7
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-10-18 22:07:04 +0300

    Update version.py

--- src/version.py
+++ src/version.py
@@ -22,7 +22,7 @@ import base64
 row = []
 info = ''
 result = ''
-systemR = '1.3.21'
+systemR = '1.4.22'
 
 def sys_check():
 	print(G + '[>]' + C + ' Checking for system configurations....', end='')
