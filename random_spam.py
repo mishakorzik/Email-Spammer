128d1788e909b62a15ce1bb5cf504daaf280f7a3
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-10-18 22:06:45 +0300

    Update random_spam.py

--- random_spam.py
+++ random_spam.py
@@ -4,6 +4,4 @@ import requests
 import time
 
 requests.get("http://flyzero.000webhostapp.com/project/email-spammer/random/Ip6.php")
-os.system("python src/version.py")
-time.sleep(1)
 print('Error code 404 not found! use: python custom_spam.py')
