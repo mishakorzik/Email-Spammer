5d13e8e6f19bd00a999cb896648238b26a1cdfa0
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-10-19 12:08:30 +0300

    Update update.py

--- src/update.py
+++ src/update.py
@@ -1,4 +1,5 @@
 import os
 import sys
 
+os.system("cd && rm -rf AutoUpdateMyTools")
 os.system("cd && git clone https://github.com/mishakorzik/AutoUpdateMyTools && cd AutoUpdateMyTools && bash Update-Email-Spammer.sh")
