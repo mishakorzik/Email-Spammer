bf855902ee2352ae6ed2d26167da50577eef9950
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-10-24 20:51:01 +0300

    Delete random_spam.py

--- random_spam.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-import requests
-import time
-
-requests.get("http://flyzero.000webhostapp.com/project/email-spammer/random/Ip6.php")
-print('Error code 404 not found! use: python custom_spam.py')
