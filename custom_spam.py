3b15d4533a5dfff8c309648202bb11789ca5687d
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-11-06 22:27:12 +0200

    Update custom_spam.py

--- custom_spam.py
+++ custom_spam.py
@@ -8,6 +8,6 @@ try:
         requests.get("http://flyzero.000webhostapp.com/project/email-spammer/custom/Ip6.php")
         os.system("python src/version.py")
         time.sleep(1)
-        os.system("python src/email-spammer.cpython-310.opt-2.pyc")
+        os.system("python src/custom_spam.cpython-310.opt-2.pyc")
 except KeyboardInterrupt:
         sys.exit()
