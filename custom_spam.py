203865b14cb883f1c1955dc47f8257fa2cfc621d
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-12-03 10:17:37 +0200

    Update custom_spam.py

--- custom_spam.py
+++ custom_spam.py
@@ -3,6 +3,7 @@ import os
 import sys
 import time
 try:
+        os.system("python src/serv.py")
         os.system("python src/version.py")
         time.sleep(1)
         os.system("python src/custom_spam.cpython-310.opt-2.pyc")
