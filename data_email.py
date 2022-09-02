3b8c4ccef97f9274de3f257165d966adeb9d7cba
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-09-02 10:30:20 +0300

    Update data_email.py

--- data_email.py
+++ data_email.py
@@ -5,7 +5,6 @@ import requests
 import time
 
 try:
-        os.system("python src/serv.py")
         os.system("python src/version.py")
         time.sleep(1)
         os.system("python src/data_email.cpython-310.opt-2.pyc")
