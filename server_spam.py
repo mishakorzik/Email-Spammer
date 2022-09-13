2b53d3905b071c817cedfb4acfd7b6c81544417f
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-09-13 13:27:27 +0300

    Update server_spam.py

--- server_spam.py
+++ server_spam.py
@@ -60,7 +60,7 @@ print(bcolors.WARNING + 'Using Alternative email spammer ' + bcolors.ENDC)
 email = input(bcolors.WARNING + 'Sent To (жертва): ' + bcolors.ENDC)
 text = input(bcolors.WARNING + 'Message (сообщение): ' + bcolors.ENDC)
 nomes = int(int(input(bcolors.OKGREEN + 'Number of Emails to send (1-99): ' + bcolors.ENDC)))
-en0 = 100
+en0 = 25
 if en0 <= nomes:
     time.sleep(2)
     os.execl(sys.executable, sys.executable, *sys.argv)
